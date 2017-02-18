a095188608ba029692f541e3993601f1b4be8087
Jocelyn Hsu <hsu.joce@gmail.com>
2017-02-17 20:49:27 -0800

    Assignment 5 completed

---
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,10 @@
+(function() {
+    function AlbumCtrl() {
+        this.albumData = angular.copy(albumPicasso);
+        this.albumSongs = angular.copy(albumPicasso.songs);
+    }
+
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', AlbumCtrl);
+})();
